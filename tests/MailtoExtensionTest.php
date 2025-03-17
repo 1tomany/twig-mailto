7a4de590a8e69dd10587fc9be456dcb6972b9ad2
Vic Cherubini <vcherubini@gmail.com>
2025-03-17 17:46:47 -0500

    adds some basic tests

--- tests/MailtoExtensionTest.php
+++ tests/MailtoExtensionTest.php
@@ -5,15 +5,35 @@ namespace OneToMany\Twig\Tests;
 use OneToMany\Twig\MailtoExtension;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
+use Twig\TwigFunction;
 
 #[Group('UnitTests')]
 #[Group('TwigTests')]
 final class MailtoExtensionTest extends TestCase
 {
 
-    public function testTrue(): void
+    public function testGettingFunctions(): void
     {
-        $this->assertFalse(false);
+        $functions = new MailtoExtension()->getFunctions();
+
+        $this->assertCount(1, $functions);
+        $this->assertInstanceOf(TwigFunction::class, $functions[0]);
+        $this->assertEquals('mailto', $functions[0]->getName());
+    }
+
+    public function testGeneratingMailtoRequiresValidEmailAddress(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The email address "invalid" is invalid.');
+
+        new MailtoExtension()->generateMailto('invalid');
+    }
+
+    public function testGeneratingMailtoLowercasesEmailAddress(): void
+    {
+        $extension = new MailtoExtension();
+
+        $this->assertStringContainsString('vic@1tomany.com', $extension->generateMailto('VIC@1TOMANY.COM'));
     }
 
 }
